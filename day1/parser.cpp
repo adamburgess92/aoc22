334a6092b334a3b2ee521f6c25ad91939225caa4
Adam Burgess <adam.burgess92@gmail.com>
2024-02-09 17:34:26 +1100

    day1

---
+++ day1/parser.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "parser.h"
+
+std::vector<std::string> input_to_vec(std::string filename) {
+    // Open the file for reading
+    std::ifstream inputFile("test_data1.txt");
+
+    // Define a vector to store lines from the file
+    std::vector<std::string> lines;
+
+    // Read and store the file contents line by line
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        // Store each line in the vector
+        lines.push_back(line);
+    }
+    inputFile.close();
+
+    // // Print the contents of the vector
+    // for (const auto& line : lines) {
+    //     std::cout << line << std::endl;
+    // }
+    return lines;
+}
