7ee0fe9d6ea494150775bdffd18ef894cfa1e81e
Adam Burgess <adam.burgess92@gmail.com>
2024-02-09 21:23:34 +1100

    day 1 part 2 solved

--- day1/main2.cpp
+++ day1/main2.cpp
@@ -13,29 +13,53 @@ void replace_blanks(std::vector<std::string>& v)
     }
 }
 
-int max_calories(std::vector<std::string>& v)
+std::vector<int> calories_by_elf(std::vector<std::string>& v)
 {
-    int largest = 0;
+    std::vector<int> v_out;
     int sum = 0;
     for (int i=0; i!=v.size(); ++i){
         if (v[i]=="x"){
-            if (sum>largest){
-                largest = sum;
-            }
+            // Append sum to v_out
+            v_out.push_back(sum);
+            // reset cal_count
             sum = 0;
         } else {
             sum += std::stoi(v[i]);
         }
     }
-    return largest;
+    return v_out;
+}
+
+std::vector<int> get_top_three(std::vector<int>& v)
+{
+    std::vector<int> v_out(v.end()-3, v.end());
+    return v_out;
+}
+
+int sum_top_three(std::vector<int>& v)
+{
+    int sum = 0;
+    for (int i=0; i!=v.size(); ++i){
+        sum += v[i];
+    }
+    return sum;
 }
 
 int main() 
 {
+    // Load data to vector
+    // std::vector<std::string> v = input_to_vector("test_data.txt");
     std::vector<std::string> v = input_to_vector("data.txt");
+    // Replace blanks with "x"
     replace_blanks(v);
-    int res = max_calories(v);
+    // Get a vector, calories by elf
+    std::vector<int> cbe = calories_by_elf(v);
+    // Sort vector of calories by elf:
+    std::sort(cbe.begin(), cbe.end());
+    // Get top three (last three values in vector)
+    std::vector<int> top_three = get_top_three(cbe);
+    // Sum top 3
+    int res = sum_top_three(top_three);
     std::cout << res << std::endl;
-
     return 0;
 }
