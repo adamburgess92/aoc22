1f8845537211566b9a2a0d7b41a4f079ecf09510
Adam Burgess <adam.burgess92@gmail.com>
2024-02-10 18:53:53 +1100

    create main2.cpp file

---
+++ day2/main2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+// #include "hand.h"
+#include "parser.h"
+
+int get_final_score(std::vector<Hand>& v)
+{
+    int score = 0;
+    for (int i=0; i!=v.size(); ++i){
+        int game_score = v[i].calculate_score();
+        score += game_score;
+    }
+    return score;
+}
+
+int main()
+{
+    std::vector<Hand> d = parse_data("data.txt");
+    int final_score = get_final_score(d);
+    std::cout << final_score << std::endl;
+}
+
