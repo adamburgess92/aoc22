cb4df3db14e78817c9d9e4b99f8ccc82eb84e0d6
adamburgess9 <adam.burgess@nine.com.au>
2024-02-20 12:57:41 +1100

    Day 7 start

---
+++ day7/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "parser.h"
+
+int main() {
+    std::vector<std::string> data = parse_data("data.txt");
+
+}
+
