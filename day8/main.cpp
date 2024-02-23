566e19f2517bd1288e759ae125cbc6aa4a666221
adamburgess9 <adam.burgess@nine.com.au>
2024-02-23 16:02:44 +1100

    Day 8 solved

--- day8/main.cpp
+++ day8/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include "parser.h"
 
 // bool is_visible(int x_col, int y_row)
@@ -20,7 +21,7 @@
 // }
 
 
-// void count_visible(std::vector<std::vector<int> >& v) 
+// void count_visible(std::vector<std::vector<int> >& v)
 // {
 //     // Count outer
 //     int outer_sum = 0;
@@ -33,12 +34,12 @@
 //         }
 //     }
 
-//     // Count inner: 
+//     // Count inner:
 //     int inner_sum = 0;
 //     for (int i=1; i!=v.size()-1; ++i){
 //         for (int j=1; j!=v[0].size()-1; ++j){
 //             std::cout << "row: " << i << std::endl;
-//             std::cout << "col: " << j << std::endl; 
+//             std::cout << "col: " << j << std::endl;
 //             // Left
 //             std::vector<int> left(v[i].begin(), v[i].begin()+j);
 //             // Right
@@ -56,9 +57,9 @@
 //                 for (int l=i+1; l<v.size(); ++l){
 //                     below.push_back(v[l][j]);
 //                 }
-//             } 
+//             }
 
-//             // // Debug: 
+//             // // Debug:
 //             // std::cout << "Below" << std::endl;
 //             // for (int b=0; b<below.size(); ++b){
 //             //     std::cout << below[b] << std::endl;
@@ -92,9 +93,14 @@
 
 int viewing_distance(int curr_tree_height, std::vector<int>& v)
 {
-    int d = 1;
-    for (int i=0; i!=v.size(); ++i){
+    // If edge, return zero:
+    // if (v.empty()) {
+    //     return 0;
+    // }
+    int d = 0;
+    for (int i=d; i!=v.size(); ++i){
         if (v[i]>=curr_tree_height){
+            ++d;
             return d;
         } else {
             ++d;
@@ -108,17 +114,19 @@ void get_max_scenic_score(std::vector<std::vector<int> >& v)
     int max_scenic_score = 0;
     for (int i=0; i!=v.size(); ++i){
         for (int j=0; j!=v[0].size(); ++j){
-            // Left
+            // Left - Needs to be reversed
             std::vector<int> left(v[i].begin(), v[i].begin()+j);
+            std::reverse(left.begin(), left.end());
             // Right
             std::vector<int> right(v[i].begin()+j+1, v[i].end());
-            // Above
+            // Above -needs to be reversed
             std::vector<int> above;
             if (i>0){
                 for (int k=0; k<i; ++k){
                     above.push_back(v[k][j]);
                 }
             }
+            std::reverse(above.begin(), above.end());
             // Below
             std::vector<int> below;
             if (i<v.size()-1){
@@ -151,7 +159,7 @@ void get_max_scenic_score(std::vector<std::vector<int> >& v)
 
 int main()
 {
-    std::vector<std::vector<int> > data = parse_data("test_data.txt");
+    std::vector<std::vector<int> > data = parse_data("data.txt");
     // part 1
     // count_visible(data);
     //part 2
