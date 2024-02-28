ac9828c23d30456601b230c5a7cc86f67a73e743
adamburgess9 <adam.burgess@nine.com.au>
2024-02-28 17:27:00 +1100

    Day 10 part 2 - fix day 1

--- day10/Register.cpp
+++ day10/Register.cpp
@@ -3,14 +3,14 @@
 #include <algorithm>
 #include "Register.h"
 
-Register::Register(): cycle(0), X(1) {};
+Register::Register(): cycle(1), X(1) {};
 void Register::build_queue(const std::string&s)
 {
     // Update Queued instructions
     std::string cmd_type = s.substr(0,4);
     if (cmd_type=="noop"){
-        cycle += 1;
         queued_instructions.emplace(cycle, 0);
+        cycle += 1;
     }
     if (cmd_type=="addx"){
         // Get the int:
@@ -21,9 +21,9 @@ void Register::build_queue(const std::string&s)
             tokens.push_back(token);
         }
         int op = std::stoi(tokens[1]);
-        cycle += 2;
         queued_instructions.emplace(cycle, 0);
         queued_instructions.emplace(cycle+1, op);
+        cycle += 2;
     }
 }
 void Register::print_queue()
@@ -38,8 +38,8 @@ void Register::print_res()
 {
     int sum = 0;
     std::vector k_cycles = {20, 60, 100, 140, 180, 220};
-    for (int i=1; i!=queued_instructions.size()+1; ++i){
-        X += queued_instructions[i];
+    for (int i=1; i!=queued_instructions.size(); ++i){
+        X += queued_instructions[i-1];
         if (std::find(k_cycles.begin(), k_cycles.end(), i) != k_cycles.end()){
             std::cout << X*i << std::endl;
             sum = sum + i*X;
