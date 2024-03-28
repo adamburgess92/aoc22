12f3255a7d406fdf204abaf97644776d6a712507
adamburgess9 <adam.burgess@nine.com.au>
2024-03-28 16:26:40 +1100

    Better day 13

--- day13/main.py
+++ day13/main.py
@@ -2,7 +2,7 @@
 import itertools
 
 d = []
-with open("test_data.txt", "r") as f:
+with open("data.txt", "r") as f:
     for line in f:
         d.append(line)
 # Clean up text
@@ -16,49 +16,50 @@ right = [eval(i) for i in right]
 assert len(left)==len(right), "left and right should have same length"
 
 def check_and_convert_to_list(a):
-    if not(type(a))==list:
+    if not(isinstance(a, list)):
         return [a]
     else:
         return a
 
-def compare_single(l: list, r:list) -> bool:
-    # print(l)
-    # print(r)
-    if (len(r)==0):
-        return True
+def compare_single(l, r):
     z = itertools.zip_longest(l, r)
     for i in z:
         l, r = i
         l = check_and_convert_to_list(l)
         r = check_and_convert_to_list(r)
-        # print(l)
-        # print(r)
-        # Otherwise...
-        if (l[0]==None):
+        print(l)
+        print(r)
+        # Check lengths:
+        if (len(l)==0) & (len(r)>0):
             return True
-        if (l[0]<=r[0]):
-            compare_single(l[1:], r[1:])
+        elif (len(l)>0) & (len(r)==0):
+            return False
+        # Check Nones
+        elif (l[0] is None) & (r[0] is not None):
+            return True
+        elif (l[0] is not None) & (r[0] is None):
+            return False
+        # Check values
+        elif (l[0]<r[0]):
+            return True
+        elif (l[0]>r[0]):
+            return False
+        # If we haven't broken yet, continue...
         else:
-            # print("incorrect order")
-            # return False
-            raise RecursionError("X")
-    return True
+            result = compare_single(l[1:], r[1:])
+            if result is not None:
+                return result
 
-def compare(l:list, r:list):
-    try: 
-        b = compare_single(l, r)
-    except:
-        b = False
-    return b
 
-test_l = []
-test_r = [3]
-print(compare(test_l, test_r))
+# test_l = [7,7,7,7]
+# test_r = [7,7,7]
+# print(compare_single(test_l, test_r))
 
 res = [None]
 for i in range(0, len(left)):
     print(left[i])
-    c = compare(left[i], right[i])
+    print(right[i])
+    c = compare_single(left[i], right[i])
     res.append(c)
 
 trues = [i for i,v in enumerate(res) if v is True]
