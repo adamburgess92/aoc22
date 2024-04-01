fcde9235b45436b0194dafb8b79dfded831c96f1
adamburgess9 <adam.burgess@nine.com.au>
2024-04-01 23:35:33 +1100

    Day 13 Recursive

--- day13/main.py
+++ day13/main.py
@@ -37,7 +37,7 @@ def check_and_convert_to_list(a):
         return a
 
 def compare_single(l, r):
-    if len(l)==0: 
+    if len(l)==0:
         return True
     if len(r)==0:
         return False
@@ -72,11 +72,79 @@ def compare_single(l, r):
         if i==len(l)-1:
             return True
 
+def compare_atom(li, ri):
+    if isinstance(li, int) & isinstance(ri, int):
+            if li < ri:
+                return True
+            elif ri < li:
+                return False
+    # If both lists:
+    elif isinstance(li, list) & isinstance(ri, list):
+        for j in range(0, max(len(li), len(ri))):
+            try:
+                lij = li[j]
+            except IndexError:
+                return True # Left side ran out of items
+            try:
+                rij = ri[j]
+            except IndexError:
+                return False # Right side ran out of items
+            if compare_atom(lij, rij) is True:
+                return True
+            elif compare_atom(lij, rij) is False:
+                return False
+            # if isinstance(lij, int) & isinstance(rij, list):
+            #     return True
+            # elif isinstance(lij, list) & isinstance(rij, int):
+            #     return False
+            # elif isinstance(lij, list) & isinstance(rij, list):
+            #     pass
+            # elif isinstance(lij, int) & isinstance(rij, int):
+            #     if lij < rij:
+            #         return True
+            #     elif lij > rij:
+            #         return False
+
+    # If mix of list and int:
+    elif isinstance(li, list) & isinstance(ri, int) | isinstance(li, int) & isinstance(ri, list):
+        lil = check_and_convert_to_list(li)
+        ril = check_and_convert_to_list(ri)
+        for j in range(0, max(len(lil), len(ril))):
+            try:
+                lilj = lil[j]
+            except IndexError:
+                return True # Left side ran out of itetms
+            try:
+                rilj = ril[j]
+            except:
+                return False # Right side ran out of items
+            if compare_atom(lilj, rilj) is True:
+                return True
+            elif compare_atom(lilj, rilj) is False:
+                return False
+
+def compare(l, r):
+    for i in range(0, max(len(l), len(r))):
+        try:
+            li = l[i]
+        except IndexError:
+            return True
+        try:
+            ri = r[i]
+        except IndexError:
+            return False
+        b = compare_atom(li, ri)
+        if b is False:
+            return False
+        if b is True:
+            return True
+    return True
+
 def insertion_sort(ls):
     for i in range(1, len(ls)):
         key = ls[i]
         j = i-1
-        while j >= 0 and compare_single(key, ls[j]) :
+        while j >= 0 and compare(key, ls[j]) :
                 ls[j + 1] = ls[j]
                 j -= 1
         ls[j + 1] = key
@@ -95,8 +163,8 @@ def get_result_part_one(filename):
     for i, v in enumerate(range(0, len(left))):
         print(left[i])
         print(right[i])
-        print(compare_single(left[i], right[i]))
-        if compare_single(left[i], right[i]):
+        print(compare(left[i], right[i]))
+        if compare(left[i], right[i]):
             res.append(i+1)
     print(sum(res))
 
@@ -108,11 +176,19 @@ def get_result_part_two(filename):
     print(res)
 
 
+def test_compare():
+    l = [1,[2,[3,[4,[5,6,7]]]],8,9]
+    r = [[1],4] # Wnat this to return true
+    res = compare(l, r)
+    print(res)
+
+# test_compare()
+
 # test_s = [12, 11, 13, 5, 6]
 # r = insertion_sort(test_s)
 # print(r)
 
-get_result_part_one("day13/data.txt")
+# get_result_part_one("day13/data.txt")
 get_result_part_two("day13/test_data.txt")
 
 
