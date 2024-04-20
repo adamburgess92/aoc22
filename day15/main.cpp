a02a390c078ebb5c63f0e2d29deafe4ea3a6b62f
Adam Burgess <adam.burgess92@gmail.com>
2024-04-20 23:34:19 +1000

    Progress

--- day15/main.cpp
+++ day15/main.cpp
@@ -31,14 +31,29 @@ std::vector<std::vector<int>> apply_find_ints(const std::vector<std::string>& v_
     return v_out;
 }
 
-void solver (int s_x, int s_y, int b_x, int b_y, int line)
+std::vector<int> find_intersect(int s_x, int s_y, int b_x, int b_y, int line)
 {
     int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
     int y_dist = abs(line - s_y);
-    int x_a = s_x - (m_dist - y_dist);
-    int x_b = s_x + (m_dist - y_dist);
-    std::cout << "x_a: " << x_a << std::endl;
-    std::cout << "x_b: " << x_b << std::endl;    
+    int x_dist = abs(m_dist - y_dist);
+    int x_a = s_x - x_dist;
+    int x_b = s_x + x_dist;
+    std::vector<int> v_out;
+    v_out.push_back(x_a);
+    v_out.push_back(x_b);
+    return v_out;
+}
+
+std::vector<std::vector<int>> find_all_intersect(const std::vector<std::vector<int>>& v_in, int line)
+{
+    std::vector<std::vector<int>> v_out;
+    for (int i=0; i!=v_in.size(); ++i){
+        auto inter = find_intersect(v_in[i][0], v_in[i][1], v_in[i][2], v_in[i][3], line);
+        // std::cout << inter[0] << std::endl;
+        // std::cout << inter[1] << std::endl;
+        v_out.push_back(inter);
+    }
+    return v_out;
 }
 
 
@@ -47,13 +62,13 @@ int main ()
     std::vector<std::string> data = load_data("test_data.txt");
     auto v = apply_find_ints(data);
 
-    // idx 6 == (8,7) from the example
-    int s_x = v[6][0];
-    int s_y = v[6][1];
-    int b_x = v[6][2];
-    int b_y = v[6][3];
-
-    solver(s_x, s_y, b_x, b_y, 10);
+    // // idx 6 == (8,7) from the example
+    // int s_x = v[6][0];
+    // int s_y = v[6][1];
+    // int b_x = v[6][2];
+    // int b_y = v[6][3];
+    // find_intersect(s_x, s_y, b_x, b_y, 10);
+    auto intersections = find_all_intersect(v, 10); 
     
     return 0;
 }
