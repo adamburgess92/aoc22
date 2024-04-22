04495a9b7fdfaab38cf0df7267afd44657bfee21
adamburgess9 <adam.burgess@nine.com.au>
2024-04-22 16:48:00 +1000

    Day 15 test passed

--- day15/main.cpp
+++ day15/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <sstream> 
+#include <sstream>
 #include <regex>
 #include "Parser.h"
 
 
-std::vector<int> find_ints(const std::string& s) 
-{ 
+std::vector<int> find_ints(const std::string& s)
+{
     std::vector<int> ints;
     std::regex pattern(R"((-?\d+))");
     std::sregex_iterator it(s.begin(), s.end(), pattern);
@@ -33,6 +33,7 @@ std::vector<std::vector<int>> apply_find_ints(const std::vector<std::string>& v_
 
 std::vector<int> find_intersect(int s_x, int s_y, int b_x, int b_y, int line)
 {
+    // Find points x_a and x_b where signal instersects with line
     int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
     int y_dist = abs(line - s_y);
     int x_dist = abs(m_dist - y_dist);
@@ -46,32 +47,83 @@ std::vector<int> find_intersect(int s_x, int s_y, int b_x, int b_y, int line)
 
 std::vector<std::vector<int>> find_all_intersect(const std::vector<std::vector<int>>& v_in, int line)
 {
+    // Generate [[x_a, x_b], [x_a, x_b], ...] for all signals
     std::vector<std::vector<int>> v_out;
     for (int i=0; i!=v_in.size(); ++i){
         auto inter = find_intersect(v_in[i][0], v_in[i][1], v_in[i][2], v_in[i][3], line);
         // std::cout << inter[0] << std::endl;
         // std::cout << inter[1] << std::endl;
+        // std::cout << std::endl;
         v_out.push_back(inter);
     }
     return v_out;
 }
 
-
-int main () 
+int find_min_x(const std::vector<std::vector<int>>& v_in)
 {
-    std::vector<std::string> data = load_data("test_data.txt");
-    auto v = apply_find_ints(data);
-    auto intersections = find_all_intersect(v, 10); 
-    // std::cout << intersections[6][0] << std::endl;
-    // std::cout << intersections[6][1] << std::endl;
-    int n_possible_beacon = 0;
-    for (int i=-3; i!=26; ++i){
-        for (int j=0; j!=intersections.size(); ++j){
-            // If i between start/end then cannot contain beacon - but need to remove overlap first...
-            if
+    int min;
+    for (int i=0; i!=v_in.size(); ++i){
+        int s_x = v_in[i][0];
+        int s_y = v_in[i][1];
+        int b_x = v_in[i][2];
+        int b_y = v_in[i][3];
+        int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
+        int x_min = s_x - m_dist;
+        if (x_min < min){
+            min = x_min;
         }
     }
-    
+    return min;
+}
+
+int find_max_x(const std::vector<std::vector<int>>& v_in)
+{
+    int max=0;
+    for (int i=0; i!=v_in.size(); ++i){
+        int s_x = v_in[i][0];
+        int s_y = v_in[i][1];
+        int b_x = v_in[i][2];
+        int b_y = v_in[i][3];
+        int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
+        int x_max = s_x + m_dist;
+        if (x_max > max){
+            max = x_max;
+        }
+    }
+    return max;
+}
+
+void no_possible_beacon(int start, int stop, std::vector<std::vector<int>> intersections)
+{
+    int n;
+    for (int i=start; i!=stop; ++i){
+        for (int j=0; j!=intersections.size(); ++j){
+            int x_a = intersections[j][0];
+            int x_b = intersections[j][1];
+            if (i>x_a && i<x_b) {
+                ++n;
+                break;
+            }
+        }
+    }
+    std::cout << "res: " << n << std::endl;
+}
+
+int main ()
+{
+    std::vector<std::string> data = load_data("test_data.txt");
+    std::vector<std::vector<int>> v = apply_find_ints(data);
+    auto intersections = find_all_intersect(v, 10);
+    std::cout << intersections[6][0] << std::endl;
+    std::cout << intersections[6][1] << std::endl;
+    int x_min = find_min_x(v);
+    int x_max = find_max_x(v);
+    std::cout << "x_min: " << x_min << std::endl;
+    std::cout << "x_max: " << x_max << std::endl;
+    no_possible_beacon(x_min, x_max, intersections);
+
+
+    // Test things:
     return 0;
 }
 
@@ -79,10 +131,10 @@ int main ()
 
 
 
-/* 
+/*
 In row 2,000,000, how many positions cannot contain a beacon?
 Don't draw the grid, it'll take forever...
-This is probably a math problem 
+This is probably a math problem
 Maybe start by finding boundaries? Even that is a lot...
 
 From sensor S, if dist is d, what are the start/end coordinates at line L? <- this is probably the way
