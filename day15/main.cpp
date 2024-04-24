5a47c1fe9d86c8a1fb97534f6d49c839e3cdeef6
Adam Burgess <adam.burgess92@gmail.com>
2024-04-24 16:02:13 +1000

    Day 15 part 1 solved

--- day15/main.cpp
+++ day15/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <regex>
@@ -31,72 +32,61 @@ std::vector<std::vector<int>> apply_find_ints(const std::vector<std::string>& v_
     return v_out;
 }
 
-std::vector<int> find_intersect(int s_x, int s_y, int b_x, int b_y, int line)
+std::vector<std::vector<int>> find_intersections(const std::vector<std::vector<int>>& v_in, int line)
 {
-    // Find points x_a and x_b where signal instersects with line
-    int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
-    int y_dist = abs(line - s_y);
-    int x_dist = abs(m_dist - y_dist);
-    int x_a = s_x - x_dist;
-    int x_b = s_x + x_dist;
-    std::vector<int> v_out;
-    v_out.push_back(x_a);
-    v_out.push_back(x_b);
-    return v_out;
-}
-
-std::vector<std::vector<int>> find_all_intersect(const std::vector<std::vector<int>>& v_in, int line)
-{
-    // Generate [[x_a, x_b], [x_a, x_b], ...] for all signals
+    // v_in is [[s_x, s_y, b_x, b_y],[s_x, s_y, b_x, b_y], ...]
+    // line is the line at which to check for intersections
     std::vector<std::vector<int>> v_out;
-    for (int i=0; i!=v_in.size(); ++i){
-        auto inter = find_intersect(v_in[i][0], v_in[i][1], v_in[i][2], v_in[i][3], line);
-        // std::cout << inter[0] << std::endl;
-        // std::cout << inter[1] << std::endl;
-        // std::cout << std::endl;
-        v_out.push_back(inter);
+    for (const std::vector<int>& row : v_in){
+        int s_x = row[0];
+        int s_y = row[1];
+        int b_x = row[2];
+        int b_y = row[3];
+        int m_dist = abs(b_y-s_y) + abs(b_x-s_x); // This is correct
+        int y_dist = abs(line - s_y); 
+        if (y_dist > m_dist){
+            std::cout << "Signal at " << s_x << ", " << s_y << " does not reach row " << line << std::endl;
+            continue;
+        }
+        int x_dist = abs(y_dist-m_dist);
+        int x_a = s_x - x_dist;
+        int x_b = s_x + x_dist;
+        std::cout << "Signal at " << s_x << ", " << s_y << " meets line at " << x_a << ", " << x_b << std::endl;
+        std::vector<int> v_pair {x_a, x_b};
+        v_out.push_back(v_pair);
     }
     return v_out;
 }
 
-int find_min_x(const std::vector<std::vector<int>>& v_in)
+
+int find_start(const std::vector<std::vector<int>>& v_intersections)
 {
-    int min=0;
-    for (int i=0; i!=v_in.size(); ++i){
-        int s_x = v_in[i][0];
-        int s_y = v_in[i][1];
-        int b_x = v_in[i][2];
-        int b_y = v_in[i][3];
-        int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
-        int x_min = s_x - m_dist;
-        if (x_min < min){
-            min = x_min;
+    int min_x = 0;
+    for (int i=0; i<v_intersections.size(); ++i){
+        int a = v_intersections[i][0];
+        if (a<min_x){
+            min_x = a;
         }
     }
-    return min;
+    return min_x;
 }
 
-int find_max_x(const std::vector<std::vector<int>>& v_in)
+int find_stop(const std::vector<std::vector<int>>& v_intersections)
 {
-    int max=0;
-    for (int i=0; i!=v_in.size(); ++i){
-        int s_x = v_in[i][0];
-        int s_y = v_in[i][1];
-        int b_x = v_in[i][2];
-        int b_y = v_in[i][3];
-        int m_dist = abs(b_y-s_y) + abs(b_x-s_x);
-        int x_max = s_x + m_dist;
-        if (x_max > max){
-            max = x_max;
+    int max_x = 0;
+    for (int i=0; i<v_intersections.size(); ++i){
+        int b = v_intersections[i][1];
+        if (b>max_x){
+            max_x = b;
         }
     }
-    return max;
+    return max_x;
 }
 
 void no_possible_beacon(int start, int stop, std::vector<std::vector<int>> intersections)
 {
     int n=0;
-    for (int i=start; i<stop+12; ++i){
+    for (int i=start; i<=stop; ++i){
         for (int j=0; j!=intersections.size(); ++j){
             int x_a = intersections[j][0];
             int x_b = intersections[j][1];
@@ -104,19 +94,12 @@ void no_possible_beacon(int start, int stop, std::vector<std::vector<int>> inter
             if (i>=x_a && i<=x_b) {
                 in = true;
                 ++n;
-                if (in && i>stop){
-                    std::cout << "i: " << i << std::endl;
-                    std::cout << "x_a: " << x_a << std::endl;
-                    std::cout << "x_b: " << x_b << std::endl;
-                    std::cout << "in: " << in << std::endl;
-                    std::cout << "n: " << n << std::endl;
-                    std::cout << std::endl;
-                }
                 break;
             }
         }
     }
     std::cout << "res: " << n << std::endl;
+    std::cout << "res-1: " << n-1 << std::endl; // Because row contains a beacon already
 }
 
 int main ()
@@ -124,20 +107,16 @@ int main ()
     std::vector<std::string> data = load_data("data.txt");
     // std::cout << data[0] << std::endl;
     std::vector<std::vector<int>> v = apply_find_ints(data);
-    auto intersections = find_all_intersect(v, 2000000);
-    // auto intersections = find_all_intersect(v, 10);
-
-    int x_min = find_min_x(v);
-    int x_max = find_max_x(v);
-    std::cout << "x_min: " << x_min << std::endl;
-    std::cout << "x_max: " << x_max << std::endl;
-    no_possible_beacon(x_min, x_max, intersections);
-
-    // Test things:
+    std::vector<std::vector<int>> intersections = find_intersections(v, 2000000);
+    int start = find_start(intersections);
+    int stop = find_stop(intersections);
+    no_possible_beacon(start, stop, intersections);
     return 0;
 }
 
-// 6345457-1 is not the answer
+// 6582242 is not the answer
+// 6582243 is not the answer - too high
+// 6345457-1 is not the answer - too high
 // Weirdly - when x_max increases by 1, result is increasing by one. 
 // This is not happening when x_min is decreased
 
