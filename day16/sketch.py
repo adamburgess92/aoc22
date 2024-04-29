d2151059083d66eb39a0bf2a7ea8c673895f2a16
Adam Burgess <adam.burgess92@gmail.com>
2024-04-29 22:54:28 +1000

    Python version

---
+++ day16/sketch.py
@@ -0,0 +1,45 @@
+m = {
+    "AA": {
+        "flow_rate":0,
+        "tunnels":["DD", "II", "BB"]
+    },
+    "BB": {
+        "flow_rate":13,
+        "tunnels":["CC", "AA"]
+    },
+    "CC": {
+        "flow_rate":2,
+        "tunnels" : ["DD", "BB"]
+    },
+    "DD": {
+        "flow_rate":20,
+        "tunnels":["CC", "AA", "EE"]
+    },
+    "EE": {
+        "flow_rate":3,
+        "tunnels": ["FF", "DD"]
+    },
+    "FF": {
+        "flow_rate":0,
+        "tunnels": ["EE", "GG"]
+    },
+    "GG": {
+        "flow_rate":0,
+        "tunnels" : ["FF", "HH"]
+    },
+    "HH": {
+        "flow_rate":22, 
+        "tunnel" : ["GG"]
+    },
+    "II": {
+        "flow_rate":0, 
+        "tunnels":["AA", "JJ"]
+    },
+    "JJ": {
+        "flow_rate":21, 
+        "tunnel" : ["II"]
+    }
+}
+
+def potential_pressure_release(m, t):
+    t_remain = 30-t
