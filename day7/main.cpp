b259d62809ac002c320e2185f8fb75ca8d74a08d
Adam Burgess <adam.burgess92@gmail.com>
2024-06-21 22:41:49 +1000

    More additions - day 7

--- day7/main.cpp
+++ day7/main.cpp
@@ -10,17 +10,29 @@
 // Part 1: Find all of the directories with a total size of at most 100000.
 // What is the sum of the total sizes of those directories?
 
+struct File {
+    std::string filename;
+    int filesize;
+    // Constructor: 
+    File(std::string filename, int filesize) : filename(filename), filesize(filesize) {}
+};
+
 struct Node {
     std::string dir;
+    Node* parent;
     std::vector<Node*> children;
+    std::vector<File> files;
     // Constructor: 
     Node(std::string val) : dir(val) {};
 };
 
-class Filesystem {
-public: 
-    Node current_dir;
-};
+// class Filesystem {
+// public: 
+//     Node root_node;
+//     Node current_dir;
+//     Filesystem() {}
+    
+// };
 
 bool is_ls(std::string s_in){
     return s_in.substr(0, 4) == "$ ls";
@@ -30,7 +42,28 @@ bool is_cd(std::string s_in){
     return s_in.substr(0, 4) == "$ cd";
 }
 
-void determine_action(std::string s_in){
+void process_ls(const std::vector<std::string>& s_ls, Node* current_node){
+    for (const std::string& s : s_ls){
+        // While not another command...
+        if ((!is_ls(s)) & (!is_cd(s))){
+            // If it's a directory
+            if (s.substr(0, 3) == "dir") {
+                std::string dir_name = s.substr(s.find(" "), s.length());
+                Node* newdir = new Node(dir_name);
+                current_node->children.push_back(newdir);
+            }
+            // If it's a file...
+            else {
+                int filesize = std::stoi(s.substr(0, s.find(" ")));
+                std::string filename = s.substr(s.find(" "), s.length());
+                File newfile(filename, filesize);
+                current_node->files.push_back(newfile);
+            }
+        }
+    }
+}
+
+void perform_action(std::string s_in){
     if (is_ls(s_in)){
         //
     }
@@ -70,7 +103,7 @@ int main()
 
     // Process commands: 
     for (const std::string& s : data){
-
+        perform_action(s);
 
     }
     
