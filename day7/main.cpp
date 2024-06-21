6d147f3d19ec1680fb23011ba08ccf1c3132369f
Adam Burgess <adam.burgess92@gmail.com>
2024-06-21 22:05:57 +1000

    Start day7

--- day7/main.cpp
+++ day7/main.cpp
@@ -6,32 +6,49 @@
 #include <algorithm>
 #include "parser.h"
 
+
 // Part 1: Find all of the directories with a total size of at most 100000.
 // What is the sum of the total sizes of those directories?
 
-template<typename T>
-class TreeNode
-{
-public:
-    // Constructor
-    TreeNode() {}
-    TreeNode(const T& value): Value(value) {}
-
-    T Value;
-    std::list<TreeNode<T> > Children;
-
-    // Every "Children" needs to be appended with ".."
+struct Node {
+    std::string dir;
+    std::vector<Node*> children;
+    // Constructor: 
+    Node(std::string val) : dir(val) {};
 };
 
+class Filesystem {
+public: 
+    Node current_dir;
+    
+}
 
 int main()
 {
     std::vector<std::string> data = parse_data("test_data.txt");
-    for (int i=0; i!=data.size(); ++i){
-        std::cout << data[i][0] << std::endl;
-        if (d[i][0]=='$ cd') {
-            TreeNode(d[i][1])
-        }
-    }
+    // Print data
+    // for (const std::string& s : data) {
+    //     std::cout << s << std::endl;
+    // }
+    /*
+    A row can begin with either
+    - $ -> a command
+    - dir -> a directory
+    - 23409234 -> file size 
+    */
 
+   // Test Node struct
+   Node* root = new Node("/");
+
+   Node* child1 = new Node("foo");
+   Node* child2 = new Node("bar");
+   Node* child3 = new Node("baz");
+
+   root->children.push_back(child1);
+   root->children.push_back(child2);
+   child2->children.push_back(child3);
+
+
+   
+    
 }
