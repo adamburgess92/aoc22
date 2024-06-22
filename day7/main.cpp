05b0e814d55441018ba3bda021451f3034699d2a
Adam Burgess <adam.burgess92@gmail.com>
2024-06-22 22:43:24 +1000

    Cleanup

--- day7/main.cpp
+++ day7/main.cpp
@@ -36,12 +36,75 @@ public:
         current_directory = root;
         parent_directory = nullptr;
     }
-    // Change directory: 
-    void change_directory(std::string cd_string){
-        if (cd_string=="$ cd .."){
-            current_directory = parent_directory;
-            return;
+    void add_file(std::string s_file){
+        int filesize = std::stoi(s_file.substr(0, s_file.find(" ")));
+        std::string filename = s_file.substr(s_file.find(" ")+1, s_file.length());
+        File* newfile = new File(filename, filesize);
+        current_directory->files.push_back(newfile);
+    }
+    void add_directory(std::string s_directory){
+        std::string directory_name = s_directory.substr(s_directory.find(" ")+1, s_directory.length());
+        Directory* newdir = new Directory(directory_name);
+        current_directory->subdirectories.push_back(newdir);
+    }
+    void change_directory(std::string s_cd){
+        Directory* tmp_current; 
+        Directory* tmp_parent;
+        std::string dir_name = s_cd.substr(s_cd.find("cd")+3, s_cd.length());
+        for (int i=0; i<=current_directory->subdirectories.size(); ++i){
+            if (dir_name==current_directory->subdirectories[i]->name){
+                current_directory = current_directory->subdirectories[i];
+                // parent_directory = tmp_current->name;
+                break;
+            }
         }
+    }
+    void up_level(){
+        // Store temps: 
+        // Directory* tmp_current; 
+        // Directory* tmp_parent;
+        std::cout << "Parent directory: " << current_directory->name << std::endl;
+    }
+
+};
+
+int main()
+{
+    // Load data
+    std::vector<std::string> data = parse_data("test_data.txt");
+    // Skip first line and instantiate FileSystem with root directory:
+    FileSystem fs = FileSystem("/");
+    // $ ls - do nothing:
+    // dir a
+    fs.add_directory("dir a");
+    // 14848514 b.txt
+    fs.add_file("14848514 b.txt");
+    // 8504156 c.dat
+    fs.add_file("8504156 c.dat");
+    // dir d
+    fs.add_directory("dir d");
+    // $ cd a
+    // fs.change_directory("$ cd a");
+    // $ cd ..
+    std::cout << "current directory: " << fs.current_directory->name << std::endl;
+    // std::cout << "parent directory: " << fs.parent_directory->name << std::endl;
+    fs.change_directory("$ cd a");
+    std::cout << "current directory: " << fs.current_directory->name << std::endl;
+    
+    
+    // fs.current_directory = fs.current_directory->subdirectories[0];
+    // std::cout << "moved, current directory: " << fs.current_directory->name << std::endl;
+    // fs.up_level();
+    // std::cout << "after uplevel, current directory: " << fs.current_directory->name << std::endl;
+
+}
+
+
+
+/*    void change_directory(std::string cd_string){
+        // store temps 
+        Directory* tmp_parent = parent_directory;
+        Directory* tmp_current = current_directory;
         // Directory name: 
         std::string dir_name = cd_string.substr(cd_string.find("cd")+3, cd_string.length());
         // Check if directory already exists:
@@ -55,49 +118,16 @@ public:
         }
         // If it doesn't exist, create new directory
         if (!dir_exists) {
-            std::cout << "Creating new directory" << std::endl;
+            std::cout << "Directory " << dir_name << " does not exist - creating it" << std::endl;
             add_directory(dir_name);
         }
-        // Change directory: 
-        parent_directory = current_directory;
+        // // Change directory: 
+        // current_directory = current_directory->subdirectories
     }
-    void add_file(std::string s_file){
-        int filesize = std::stoi(s_file.substr(0, s_file.find(" ")));
-        std::string filename = s_file.substr(s_file.find(" ")+1, s_file.length());
-        File* newfile = new File(filename, filesize);
-        current_directory->files.push_back(newfile);
-    }
-    void add_directory(std::string s_directory){
-        std::string directory_name = s_directory.substr(s_directory.find(" ")+1, s_directory.length());
-        Directory* newdir = new Directory(directory_name);
-        current_directory->subdirectories.push_back(newdir);
-    }
-};
-
-int main()
-{
-    // std::vector<std::string> data = parse_data("test_data.txt");
-    
-    FileSystem fs = FileSystem("/");
-    std::cout << fs.current_directory->name << std::endl;
-    // $ ls - do nothing:
-    // dir a
-    fs.add_directory("dir a");
-    // 14848514 b.txt
-    fs.add_file("14848514 b.txt");
-    
-
-
-
-    /*
-    // 8504156 c.dat
-    current_directory = add_file("8504156 c.dat", current_directory);
-    // dir d
-    current_directory = add_directory("dir d", current_directory);
-    // $ cd a
-    change_directory("$ cd a", current_directory);
-    change_directory("$ cd ..", current_directory);
-    */
-
-
-}
+    void up_level(){
+        // store temps 
+        Directory* tmp_parent = parent_directory;
+        Directory* tmp_current = current_directory;
+        current_directory = tmp_current;
+        parent_directory = 
+    }*/
